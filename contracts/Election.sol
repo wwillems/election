30bdc497adc3dbad565051602ae3432d01512635
wwillems <william.willems@gmail.com>
2018-12-19 09:26:00 +0100

    initial test

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,25 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.2;
 
 contract Election {
 
-	string public candidate;
+	struct Candidate {
+		uint256 id;
+		string name;
+		uint256 voteCount;
+	}
+
+	mapping(uint => Candidate) public candidates;
+
+	uint public candidatesCount;
 
 	constructor() public {
-		candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate(string _name) private {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 
 }
